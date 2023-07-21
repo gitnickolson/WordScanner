372c3735b36fb0643bc70cb8993944db55fcb3f0
gitnickolson <nicknickolson@web.de>
2023-07-21 12:59:37 +0200

    Small changes

--- Main/main.rb
+++ Main/main.rb
@@ -1,59 +1,56 @@
 # frozen_string_literal: true
 
-class Main
-  def palindrometest
-    puts "Which word would you like me to test?"
-    palindrome = gets.chomp
-    wordreverse = palindrome.reverse
+def palindrome_test
+  puts 'Which word would you like me to test?'
+  palindrome = gets.chomp
+  word_reverse = palindrome.reverse
 
-    if wordreverse == palindrome then
-      puts "#{palindrome} is a palindrome."
+  if word_reverse == palindrome
+    puts "#{palindrome} is a palindrome."
 
-    else
-      puts "#{palindrome} is not a palindrome."
+  else
+    puts "#{palindrome} is not a palindrome."
 
-    end
   end
-
-  def anagramtest
-    puts "This will require you to enter 2 words for me to compare."
-    puts "What's the first word?"
-    anagram1 = gets.chomp
-    puts "What's the second word?"
-    anagram2 = gets.chomp
-
-    anagram11 = anagram1.chars
-    anagram22 = anagram2.chars
-
-    sortedword1 = anagram11.sort
-    sortedword2 = anagram22.sort
-
-    if sortedword1 == sortedword2
-      puts "These words are Anagrams."
-
-    else
-      puts "These words aren't Anagrams."
-
-    end
-  end
-
-  def choicemethod
-    puts "What should I test for?"
-    choice = gets.chomp
-    if choice.start_with?("Pali", "pali") then
-      palindrometest
-
-    elsif choice.start_with?("Ana", "ana") then
-      anagramtest
-
-    elsif !choice.start_with?("Ana", "ana", "Pali", "pali") then
-      puts "Please input if I should either test for an Anagram or a Palindrome."
-      choicemethod
-
-    end
-  end
-
-  ######################################################################################################################
-
-  choicemethod
 end
+
+def anagram_test
+  puts 'This will require you to enter 2 words for me to compare.'
+  puts "What's the first word?"
+  anagram = gets.chomp
+  puts "What's the second word?"
+  anagram1 = gets.chomp
+  anagram2 = anagram.chars
+  anagram3 = anagram1.chars
+
+  sorted_word = anagram2.sort
+  sorted_word1 = anagram3.sort
+
+  if sorted_word == sorted_word1
+    puts 'These words are Anagrams.'
+
+  else
+    puts "These words aren't Anagrams."
+
+  end
+end
+
+def choice_method
+  puts 'What should I test for?'
+  choice = gets.chomp
+  if choice.start_with?('Pali', 'pali')
+    palindrome_test
+
+  elsif choice.start_with?('Ana', 'ana')
+    anagram_test
+
+  elsif !choice.start_with?('Ana', 'ana', 'Pali', 'pali')
+    puts 'Please input if I should either test for an Anagram or a Palindrome.'
+    choice_method
+
+  end
+end
+
+########################################################################################################################
+
+choice_method
